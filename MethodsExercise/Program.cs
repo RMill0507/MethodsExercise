33ecfd27caa0478c8a0f2a92991792e231ad95f7
Rob <Rmilla445@gmail.com>
2022-10-14 14:12:20 -0400

    created conversation

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Name: Rob
+            //Favorite Color: Green
+            //Favorite Animal: Grizzly Bear
+            //Favorite Artist: George Strait
+
+            Console.WriteLine("Hello, What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awsome color. What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Awesome! What is your favorite artist?");
+            var artist = Console.ReadLine();
+
         }
     }
 }
